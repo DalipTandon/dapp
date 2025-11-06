c5d70c8c114f9ff5e376874beaf6a6fd328f0f4e
DalipTandon <daliptandon8@gmail.com>
2025-11-06 23:03:29 +0530

    add airdrop component

--- src/Airdrop.jsx
+++ src/Airdrop.jsx
@@ -1,8 +1,22 @@
-
-
+import React from 'react';
+import { useWallet, useConnection } from '@solana/wallet-adapter-react';
+import { use } from 'react';
 export function Airdrop() {
+    const wallet = useWallet();
+    const { connection } = useConnection();
+    async function handleAirdrop() {
 
-    return<div>
-        Airdrop Component
+        await connection.requestAirdrop(wallet.publicKey, 10);
+        alert("Airdrop Successful");
+
+    }
+
+    return <div className="h-[70vh] w-[70vw] mt-12 flex flex-col space-y-4 items-center">
+
+        <h4 className=' text-5xl font-bold text-black-400'>💦Soul Faucet💦</h4>
+        <h5 className='font-semibold text-3xl '>Have a drink! The premium faucet for Solana Devnet and Testnet.</h5>
+        <h5 className='font-semibold text-2xl '>This tool does *NOT* give real $SOL or Solana tokens.</h5>
+        <input type="text" className='border border-gray-300 p-3 rounded-2xl w-4/5 text-center' placeholder="Enter Amount to Airdrop" />
+        <button onClick={handleAirdrop} className='border border-gray-300 w-1/4 p-2 rounded-2xl bg-blue-100' >Request Airdrop </button>
     </div>
 };
--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,47 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import React, { FC, useMemo } from 'react';
+import React, { useMemo ,useState} from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { 
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+  TorusWalletAdapter
+} from '@solana/wallet-adapter-wallets';
 import {
   WalletModalProvider,
   WalletDisconnectButton,
   WalletMultiButton
 } from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
-// Default styles that can be overridden by your app
 import '@solana/wallet-adapter-react-ui/styles.css';
 import { Airdrop } from './Airdrop';
 
 function App() {
+  const network = WalletAdapterNetwork.Devnet;  //this is used to define the network
+
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  // Initialize wallet adapters
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter(),
+      new TorusWalletAdapter()
+    ],
+    [network]
+  );
 
   return (
-    <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
-      <WalletProvider wallets={[]} autoConnect>
+   <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
         <WalletModalProvider>
-          <h1>My Solana DApp</h1>
-        <Airdrop/>
+          <div className="h-screen  flex flex-col items-center justify-center space-y-6">
+            <WalletMultiButton />
+            <WalletDisconnectButton />
+            <Airdrop />
+          </div>
         </WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
